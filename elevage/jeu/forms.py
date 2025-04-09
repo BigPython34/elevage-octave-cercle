1c8b3cb6d2d1217bbebc9e24aa98b2fd0ce0f0e5
CERCLE Octave <o.cercle@pcb61-12-14.isae.fr>
2025-04-09 15:35:24 +0200

    création du fichier forms

---
+++ elevage/jeu/forms.py
@@ -0,0 +1,10 @@
+
+from django import forms
+
+class InitialisationForm(forms.Form):
+    nb_lapins_males = forms.IntegerField(label='Nombre de mâles', min_value=0)
+    nb_lapins_femelles = forms.IntegerField(label='Nombre de femelles', min_value=0)
+    nb_lapins_reproducteurs = forms.IntegerField(label='Nombre de lapins reproducteurs', min_value=0)
+    nourriture = forms.IntegerField(label='Quantité de nourriture', min_value=0)
+    nb_cages = forms.IntegerField(label='Nombre de cages', min_value=0)
+    argent = forms.DecimalField(label='Argent ', min_value=0, max_digits=10, decimal_places=2)
