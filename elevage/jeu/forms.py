d014bbca7f7443fffaccde68c1668cf0bd385124
CERCLE Octave <o.cercle@pcb61-12-14.isae.fr>
2025-04-09 16:31:04 +0200

    correction du formulaire et donc des dépendances: le formulaire ne modifiait pas la bdd, j'avais pas mis save (la honte)

--- elevage/jeu/forms.py
+++ elevage/jeu/forms.py
@@ -5,7 +5,6 @@ class InitialisationForm(forms.Form):
     nom=forms.CharField(label="Nom de l'élevage")
     nb_lapins_males = forms.IntegerField(label='Nombre de mâles', min_value=0)
     nb_lapins_femelles = forms.IntegerField(label='Nombre de femelles', min_value=0)
-    nb_lapins_reproducteurs = forms.IntegerField(label='Nombre de lapins reproducteurs', min_value=0)
     nourriture = forms.IntegerField(label='Quantité de nourriture', min_value=0)
     nb_cages = forms.IntegerField(label='Nombre de cages', min_value=0)
     argent = forms.IntegerField(label='Argent ', min_value=0, )
--- elevage/jeu/views.py
+++ elevage/jeu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import InitialisationForm
-
+from .models import Elevage
 def confirmation(request):
 
     return render(request, 'jeu/confirmation.html')  
@@ -11,17 +11,22 @@ def nouveau(request):
         if form.is_valid():
             nb_lapins_males = form.cleaned_data['nb_lapins_males']
             nb_lapins_femelles = form.cleaned_data['nb_lapins_femelles']
-            nb_lapins_reproducteurs = form.cleaned_data['nb_lapins_reproducteurs']
+            
             nourriture = form.cleaned_data['nourriture']
             nb_cages = form.cleaned_data['nb_cages']
             argent = form.cleaned_data['argent']
+            nom=form.cleaned_data['nom']
+            elevage = Elevage(
+            nom=nom,
+            nombre_lapins_males=nb_lapins_males,
+            nombre_lapins_femelles=nb_lapins_femelles,
+            nourriture=nourriture,
+            cages=nb_cages,
+            argent=argent
+            )
 
-            request.session['nb_lapins_males'] = nb_lapins_males
-            request.session['nb_lapins_femelles'] = nb_lapins_femelles
-            request.session['nb_lapins_reproducteurs'] = nb_lapins_reproducteurs
-            request.session['nourriture'] = nourriture
-            request.session['nb_cages'] = nb_cages
-            request.session['argent'] = argent
+            # Sauvegarder l'objet Elevage dans la base de données
+            elevage.save()
 
             return redirect('jeu:confirmation')  
     else:
