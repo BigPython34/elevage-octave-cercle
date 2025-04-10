ec934d0095e0ba968445202a1b85fa740d50bcba
CERCLE Octave <o.cercle@pcb61-12-14.isae.fr>
2025-04-10 11:18:36 +0200

    ajout de la classe regle

--- elevage/jeu/models.py
+++ elevage/jeu/models.py
@@ -8,8 +8,9 @@ class Elevage(models.Model):
     argent = models.IntegerField(default=0)  
     cages = models.IntegerField(default=0)  
     nom = models.CharField(max_length=100, unique=True,default="Nom par défaut")
+    
     def __str__(self):
-        return self.nom+f"  avec {self.nombre_lapins_males} mâles, {self.nombre_lapins_femelles} femelles, {self.cages} cages."
+        return self.nom
 
 
 class Individu(models.Model):
@@ -29,3 +30,22 @@ class Individu(models.Model):
     sexe = models.CharField(max_length=1, choices=SEXE_CHOICES)
     age = models.IntegerField()  
     etat = models.CharField(max_length=7, choices=ETAT_CHOICES, default='present')
+    
+class Regle(models.Model):
+    prix_nourriture = models.FloatField(default=0.5)  
+    prix_cage = models.IntegerField(default=50)      
+    prix_vente_lapin = models.IntegerField(default=10)  
+
+    consommation_m1 = models.FloatField(default=0.0)   
+    consommation_m2 = models.FloatField(default=0.1)   
+    consommation_m3 = models.FloatField(default=0.25)  
+
+    max_par_portee = models.IntegerField(default=4)
+    max_individus_par_cage = models.IntegerField(default=6)
+
+    age_min_gravide = models.IntegerField(default=6)    
+    age_max_gravide = models.IntegerField(default=60)   
+    duree_gestation = models.IntegerField(default=1)    
+
+    def __str__(self):
+        return "Règles du jeu"
