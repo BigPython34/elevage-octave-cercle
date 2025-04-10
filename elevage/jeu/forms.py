1c9f4419110da0f884c442d1318c54618aa562ef
CERCLE Octave <o.cercle@pcb61-12-14.isae.fr>
2025-04-10 11:04:15 +0200

    ajout du form tour dans forms

--- elevage/jeu/forms.py
+++ elevage/jeu/forms.py
@@ -8,3 +8,16 @@ class InitialisationForm(forms.Form):
     nourriture = forms.IntegerField(label='Quantité de nourriture', min_value=0)
     nb_cages = forms.IntegerField(label='Nombre de cages', min_value=0)
     argent = forms.IntegerField(label='Argent ', min_value=0, )
+
+class TourActionForm(forms.Form):
+    vendre_males = forms.IntegerField(min_value=0, required=False, label="Lapins mâles à vendre")
+    vendre_femelles = forms.IntegerField(min_value=0, required=False, label="Lapins femelles à vendre")
+    acheter_nourriture = forms.IntegerField(min_value=0, required=False, label="Quantité de nourriture à acheter (en g)")
+    acheter_cages = forms.IntegerField(min_value=0, required=False, label="Nombre de cages à acheter")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        for field in ['vendre_males', 'vendre_femelles', 'acheter_nourriture', 'acheter_cages']:
+            if cleaned_data.get(field) is None:
+                cleaned_data[field] = 0
+        return cleaned_data
